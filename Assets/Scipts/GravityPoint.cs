20db6f440bc5b77be8677a048f28df11288918f3
KoenBos <koenbos@outlook.com>
2024-02-13 00:59:56 +0100

    Gravity nu bijna helemaal goed

--- Assets/Scipts/GravityPoint.cs
+++ Assets/Scipts/GravityPoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GravityPoint : MonoBehaviour
 {
-    public float gravityScale, planetRadius;
+    public float gravityScale, planetRadius, gravityMinRange, gravityMaxRange;
     // Start is called before the first frame update
     //ps mini blastoff
     //https://www.youtube.com/watch?v=sK8FrL3ZJi0
@@ -21,7 +21,22 @@ public class GravityPoint : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D obj)
     {
-        Vector3 dir  = (transform.position - obj.transform.position) * gravityScale;
+        float gravitationalPower = gravityScale;
+        float dist = Vector2.Distance(obj.transform.position, transform.position);
+
+        if (dist > (planetRadius + gravityMinRange))
+        {
+            float min = planetRadius + gravityMinRange;
+            gravitationalPower = (((min + gravityMaxRange) - dist) / gravityMaxRange) * gravityScale;
+        }
+        Vector2 dir  = (transform.position - obj.transform.position) * gravityScale;
         obj.GetComponent<Rigidbody2D>().AddForce(dir);
-    }
+
+
+        //Draait de player naar de planeet
+        if (obj.CompareTag("Player"))
+        {
+            obj.transform.up = Vector3.MoveTowards(obj.transform.up, -dir, planetRadius * Time.deltaTime);
+        }
+    } 
 }
--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body; 
+    private Rigidbody2D body;
+    public float moveSpeed, jumpPower;
+    private bool isGrounded;
+    private float horizontal;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,43 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            body.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        body.AddForce(transform.right * horizontal * moveSpeed);
+    }
+//-------------------------------------------------
+    void OnTriggerStay2D(Collider2D obj)
+    {
+       if (obj.CompareTag("Planet"))
+       {
+            body.drag = 1f;
+
+            float distance = Mathf.Abs(obj.GetComponent<GravityPoint>().planetRadius - Vector2.Distance(transform.position, obj.transform.position));
+            if (distance < 0.6f)
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+       }
+    }
+
+    void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Planet"))
+        {
+            body.drag = 0.2f;
+        }
     }
 }
